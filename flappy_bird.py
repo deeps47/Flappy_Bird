f776cd2efbbf24827b7f8c01a2c24c490d922e7c
Conqueror14M <dy6579@srmist.edu.in>
2021-08-27 23:11:41 +0530

    fixed falling animation

--- flappy_bird.py
+++ flappy_bird.py
@@ -87,11 +87,16 @@ def draw_window(win, bird):
 def main():
 	bird = Bird(200, 200)
 	win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+	clock = pygame.time.Clock()
+
 	run = True
 	while run:
+		clock.tick(30)
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				run = False
+
+		bird.move()
 		draw_window(win, bird)
 	pygame.quit()
 	quit()
