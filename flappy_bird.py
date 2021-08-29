e54e4b0b689079bc3f2d5e13175b1f9faefa8762
Conqueror14M <dy6579@srmist.edu.in>
2021-08-30 00:12:25 +0530

    implemented NEAT

--- flappy_bird.py
+++ flappy_bird.py
@@ -218,3 +218,19 @@ def main():
 
 
 main()
+
+def run(config_path):
+	config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+
+	p = neat.Population(config)
+
+	p.add_reporter(neat.StdOutReporter(True))
+	stats = neat.StatisticsReporter()
+	p.add_reporter(stats)
+
+	winner = p.run(main, 100)
+
+if __name__ == "__main__":
+	local_dir = os.path.dirname(__file)
+	config_path = os.path.join(local_dir, "config-feed.txt")
+	run(config_path)
