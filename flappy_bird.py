1a56c5d4eeb9ba328aea9d589ea79e4e7748e5f8
Conqueror14M <dy6579@srmist.edu.in>
2021-08-31 18:02:22 +0530

    added gen and alive count

--- flappy_bird.py
+++ flappy_bird.py
@@ -9,6 +9,8 @@ pygame.font.init()
 WIN_WIDTH = 500
 WIN_HEIGHT = 800
 
+GEN = 0
+
 BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
 PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
@@ -157,14 +159,20 @@ class Base:
 		win.blit(self.IMG, (self.x2, self.y))
 
 
-def draw_window(win, birds, pipes, base, score):
-	win.blit(BG_IMG, (0, 0))
+def draw_window(win, birds, pipes, base, score, gen):
+	win.blit(BG_IMG, (0, 0)),
 
 	for pipe in pipes:
 		pipe.draw(win)
 
 	text = STAT_FONT.render("Score: " + str(score), 1, (255,255,255))
 	win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
+
+	text = STAT_FONT.render("Gen: " + str(gen), 1, (255,255,255))
+	win.blit(text, (10, 10))
+
+	score_label = STAT_FONT.render("Alive: " + str(len(birds)),1,(255,255,255))
+	win.blit(score_label, (10, 50))
 
 	base.draw(win)
 
@@ -173,6 +181,8 @@ def draw_window(win, birds, pipes, base, score):
 	pygame.display.update()
 
 def main(genomes, config):
+	global GEN
+	GEN += 1
 	score = 0 
 
 	nets = []
@@ -250,7 +260,7 @@ def main(genomes, config):
 				ge.pop(x)
 			
 		base.move()
-		draw_window(win, birds, pipes, base, score)
+		draw_window(win, birds, pipes, base, score, GEN)
 
 
 def run(config_path):
